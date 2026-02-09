98a95440759e563c3f963df1cd381a6dd485e471
kavyasripalanisamy466-tech <kavyasripalanisamy466@gmail.com>
2026-02-09 15:42:01 +0530

    Add files via upload

---
+++ topic 2 program 2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int num1 =5;
+    int num2 =15;
+    int num3 =20;
+    int num4 =40;
+    printf("%d ",num1);
+    printf("%d\n",num2);
+    printf("%d ",num3);
+    printf("%d ",num4);
+    return 0;
+}
