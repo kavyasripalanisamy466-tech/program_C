f0e35524bacf42ea4e886fe59d7562342a4c4459
kavyasripalanisamy466-tech <kavyasripalanisamy466@gmail.com>
2026-02-10 13:15:28 +0530

    Add files via upload

---
+++ topic 3 program 9.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    float b=a;
+    printf("%0.1f",b);
+    int c;
+    scanf("%d",&c);
+    float d=c;
+    printf("%0.f",d);
+    
+    return 0;
+}
