8662e1934fdf3162408c73aaca908b3c8ff0f315
kavyasripalanisamy466-tech <kavyasripalanisamy466@gmail.com>
2026-02-10 13:38:22 +0530

    Add files via upload

---
+++ topic 3 program 12.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a=8;
+    int b=3;
+    scanf("%d %d",&a,&b);
+    printf("%d",a-b);
+    int c=10;
+    int d=5;
+    scanf("%d %d",&c,&d);
+    printf("%d",c-d);
+    return 0;
+}
