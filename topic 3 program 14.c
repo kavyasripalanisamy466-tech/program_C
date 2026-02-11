9e5ee6c2c9f4a0189ae391885a979b5a7b975fc1
kavyasripalanisamy466-tech <kavyasripalanisamy466@gmail.com>
2026-02-11 09:39:50 +0530

    Add files via upload

---
+++ topic 3 program 14.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a=9;
+    int b=2;
+    int c=10;
+    int d=3;
+    printf("Quotient %d Remainder %d\n",a/b,a%b);
+    printf("Quotient %d Remainder %d",c/d,c%d);
+    return 0;
+}
